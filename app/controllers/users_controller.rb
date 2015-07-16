615645423a3b4a030d210550268e3cab69aa024b
Lukasz Pawlik <lukasz.pawlik@polcode.net>
2015-07-16 15:11:05 +0200

    Add crud actions to controller. Add edit page(to do).

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,29 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
   expose(:users)
+  expose(:user)
+
+  def index
+  end
 
   def show
   end
 
-  def index
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+
+  end
+
+  def destroy
+    user.destroy
+    redirect_to users_path, notice: 'User was sucessfully destroyed'
   end
 end
