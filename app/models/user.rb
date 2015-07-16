ee37765e3c09a52e00e1902c4d3aaac6c6d0abf6
Lukasz Pawlik <lukasz.pawlik@polcode.net>
2015-07-16 13:42:49 +0200

    Add some styling to signup page and navbar.

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,7 @@ class User < ActiveRecord::Base
   has_one  :student_division, foreign_key: 'student_id', class_name: 'Division'
   has_many :teacher_subject,   foreign_key: 'teacher_id', class_name: 'Subject'
   has_many :student_grades,    foreign_key: 'student_id', class_name: 'Grade'
+
+  validates :firstname, presence: true
+  validates :lastname, presence: true
 end
