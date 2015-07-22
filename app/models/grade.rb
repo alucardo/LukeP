5df247f0991c9d478d705ee8499e35f2cfbeb7b3
Lukasz Pawlik <lukasz.pawlik@polcode.net>
2015-07-22 10:30:55 +0200

    Add teacher_grade scope.

--- app/models/grade.rb
+++ app/models/grade.rb
@@ -1,4 +1,8 @@
 class Grade < ActiveRecord::Base
   belongs_to :subject
   belongs_to :student, class_name: 'User', foreign_key: 'student_id'
+  has_one :teacher, class_name: 'User', through: :subjects
+
+  scope :student_grades, ->(student_id) { where(student_id: student_id) }
+  scope :teacher_grades, ->(teacher_id) { where}
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,4 +4,5 @@ RSpec.describe User, type: :model do
     it{ expect(subject).to have_one(:student_division) }
     it{ expect(subject).to have_many(:teacher_subject) }
     it{ expect(subject).to have_many(:student_grades) }
+    it{ expect(subject).to have_many(:grades) }
 end
